550198654e4eccf51ec9a6341319c99d1e1632c7
Adriano74f <adriano.ferreira-da-silva@lycee-faure.fr>
2025-10-02 11:35:09 +0200

    fix BienRepository with Singleton and Inject method

--- app/src/main/java/com/example/agence_immo/data/repository/BienRepository.java
+++ app/src/main/java/com/example/agence_immo/data/repository/BienRepository.java
@@ -1,13 +1,18 @@
 package com.example.agence_immo.data.repository;
 
-
 import com.example.agence_immo.data.model.BienImmobilier;
 
 import java.util.ArrayList;
 import java.util.List;
+import javax.inject.Inject;
+import javax.inject.Singleton;
 
+@Singleton
 public class BienRepository {
     private final List<BienImmobilier> biens = new ArrayList<>();
+
+    @Inject
+    public BienRepository() {} // Hilt injecte automatiquement
 
     public List<BienImmobilier> getBiens() {
         return biens;
