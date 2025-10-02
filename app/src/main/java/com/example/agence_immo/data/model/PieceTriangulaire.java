ad9796a4517e11e01e0b36995cc7d14d06c87248
Adriano74f <adriano.ferreira-da-silva@lycee-faure.fr>
2025-10-02 10:23:44 +0200

    feat PieceTriangulaire

--- app/src/main/java/com/example/agence_immo/data/model/PieceTriangulaire.java
+++ app/src/main/java/com/example/agence_immo/data/model/PieceTriangulaire.java
@@ -1,4 +1,28 @@
 package com.example.agence_immo.data.model;
 
-public class PieceTriangulaire {
+public class PieceTriangulaire extends Piece {
+    private double base;
+    private double hauteur;
+
+    public PieceTriangulaire(TypePiece typePiece, String niveau, double base, double hauteur) {
+        super(typePiece, niveau);
+        this.base = base;
+        this.hauteur = hauteur;
+    }
+    public double getBase() {
+        return base;
+    }
+    public double getHauteur() {
+        return hauteur;
+    }
+    public void setBase(double base) {
+        this.base = base;
+    }
+    public void setHauteur(double hauteur) {
+        this.hauteur = hauteur;
+    }
+    @Override
+    public double surface() {
+        return (base * hauteur) / 2;
+    }
 }
