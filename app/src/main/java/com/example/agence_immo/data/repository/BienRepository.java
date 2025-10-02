70cb0b7c4d065456f7b21aec7dcb9fb170d823d0
Adriano74f <adriano.ferreira-da-silva@lycee-faure.fr>
2025-10-02 11:30:44 +0200

    feat BienRepository

--- app/src/main/java/com/example/agence_immo/data/repository/BienRepository.java
+++ app/src/main/java/com/example/agence_immo/data/repository/BienRepository.java
@@ -1,4 +1,26 @@
 package com.example.agence_immo.data.repository;
 
+
+import com.example.agence_immo.data.model.BienImmobilier;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class BienRepository {
+    private final List<BienImmobilier> biens = new ArrayList<>();
+
+    public List<BienImmobilier> getBiens() {
+        return biens;
+    }
+
+    public void addBien(BienImmobilier bien) {
+        biens.add(bien);
+    }
+
+    public BienImmobilier getBienById(String id) {
+        for (BienImmobilier b : biens) {
+            if (b.getId().equals(id)) return b;
+        }
+        return null;
+    }
 }
