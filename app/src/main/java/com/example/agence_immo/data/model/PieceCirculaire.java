32aad360bdd38964c6cb77eb1b1ca023f1a48d62
Adriano74f <adriano.ferreira-da-silva@lycee-faure.fr>
2025-10-02 10:22:48 +0200

    feat PieceCirculaire

--- app/src/main/java/com/example/agence_immo/data/model/PieceCirculaire.java
+++ app/src/main/java/com/example/agence_immo/data/model/PieceCirculaire.java
@@ -1,4 +1,23 @@
 package com.example.agence_immo.data.model;
 
-public class PieceCirculaire {
+public class PieceCirculaire extends Piece {
+    private double rayon;
+
+    public PieceCirculaire(TypePiece typePiece, String niveau, double rayon) {
+        super(typePiece, niveau);
+        this.rayon = rayon;
+    }
+
+    public double getRayon() {
+        return rayon;
+    }
+
+    public void setRayon(double rayon) {
+        this.rayon = rayon;
+    }
+
+    @Override
+    public double surface() {
+        return Math.PI * rayon * rayon;
+    }
 }
